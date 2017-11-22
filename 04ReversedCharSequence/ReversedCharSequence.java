3500022ef42e0fe207c52c70bda22aaf8d8a6daa
DenChen11214 <dchen22@stuy.edu>
2017-11-22 14:45:41 -0500

    ReversedCharSequence

---
+++ 04ReversedCharSequence/ReversedCharSequence.java
@@ -0,0 +1,19 @@
+public class ReversedCharSequence implements CharSequence{
+    private String chars;
+    public ReversedCharSequence(String characters){
+	String reverse = "";
+	for(int i = characters.length();i > 0;i--){
+	    reverse += characters.charAt(i - 1);
+	}
+	chars = reverse;
+    }
+    public char charAt(int index){
+	return chars.charAt(index);
+    }
+    public int length(){
+	return chars.length();
+    }
+    public CharSequence subSequence(int start, int end){
+	return chars.substring(start,end);
+    }
+}
---
+++ 04ReversedCharSequence/ReversedCharSequenceDriver.java
@@ -0,0 +1,6 @@
+public class ReversedCharSequenceDriver{
+    public static void main(String args[]){
+	ReversedCharSequence str = new ReversedCharSequence("Herro");
+	System.out.println(str);
+    }
+}
