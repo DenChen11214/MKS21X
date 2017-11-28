04d1d8f55224b1a5ad42df4a55179749c6d77904
DenChen11214 <dchen22@stuy.edu>
2017-11-28 14:42:43 -0500

    setter for callnumber

--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -4,4 +4,11 @@ public abstract class LibraryBook extends Book implements Comparable<LibraryBook
 	super(author,title,isbn);
 	this.callNumber = callNumber;
     }
+    public String getCallNumber(){
+	return callNumber;
+    }
+    public boolean setCallNumber(String callNumber){
+	this.callNumber = callNumber;
+	return true;
+    }
 }
