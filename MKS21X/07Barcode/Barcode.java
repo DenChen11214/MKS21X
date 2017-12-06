2fda4018d91b82c1802337e6c610230e7e6f30c6
Dennis Chen <dchen22@stuy.edu>
2017-12-05 19:32:07 -0500

    constructor and toCode method, not tested yet

---
+++ MKS21X/07Barcode/Barcode.java
@@ -0,0 +1,32 @@
+public class Barcode{
+  private String zip;
+  public Barcode(){
+    this.zip = zip;
+    toCode(zip);
+  }
+  public static String toCode(String zip){
+    String ints = "0123456789";
+    String[] digitBarcode = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
+    int checkDigit = 0;
+    String barcode = "|";
+    if (zip.length() != 5){
+      throw new IllegalArgumentException();
+    }
+    for(int i = 0; i < zip.length(); i++){
+      if(!ints.contains(zip.substring(i,i+1))){
+        throw new IllegalArgumentException();
+      }
+    }
+    for(int i = 0; i < digitBarcode.length; i++){
+      for(int n = 0;n < zip.length();n++){
+        if(zip.substring(n,n+1).equals(Integer.toString(i))){
+          barcode += digitBarcode[i];
+          checkDigit += i;
+        }
+      }
+    }
+    checkDigit = checkDigit % 10;
+    barcode += digitBarcode[checkDigit] + "|";
+    return barcode;
+  }
+}
