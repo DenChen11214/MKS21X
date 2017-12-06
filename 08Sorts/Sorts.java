2f1a9fe6dc451a22697c401628226ed7cb511d58
Dennis Chen <dchen22@stuy.edu>
2017-12-06 18:49:05 -0500

    created insertionSort and tested a few cases

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -20,12 +20,27 @@ public class Sorts{
       System.out.println(ary[minIndex]);
     }
   }
+  public static void insertionSort(int[] ary){
+    for(int i = 1;i < ary.length;i++){
+      for(int n = i - 1;n >= 0 ; n--){
+        int temp = ary[n];
+        if(ary[n + 1] < temp){
+          ary[n] = ary[n + 1];
+          ary[n + 1] = temp;
+        }
+      }
+    }
+  }
   public static void main(String[] args){
     int[] arry = {65,25,12,22,11};
     int[] arry2 = {65,25,12,25,12};
     selectionSort(arry);
     System.out.println(Arrays.toString(arry));
     selectionSort(arry2);
+    System.out.println(Arrays.toString(arry2));
+    insertionSort(arry);
+    System.out.println(Arrays.toString(arry));
+    insertionSort(arry2);
     System.out.println(Arrays.toString(arry2));
   }
 }
