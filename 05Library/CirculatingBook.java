223377b95e208e5c2414455dbbd3dcad7df39337
Dennis Chen <dchen22@stuy.edu>
2017-12-06 21:47:26 -0500

    created get and set methods

---
+++ 05Library/CirculatingBook.java
@@ -0,0 +1,30 @@
+public class CirculatingBook extends LibraryBook{
+  private String currentHolder;
+  private String dueDate;
+  public CirculatingBook(String author, String title, String isbn, String callnumber){
+    super(author,title,isbn,callnumber);
+  }
+  public String getCurrentHolder(){
+    return currentHolder;
+  }
+  public String getDueDate(){
+    return dueDate;
+  }
+  public boolean setCurrentHolder(String currentHolder){
+    this.currentHolder = currentHolder;
+    return true;
+  }
+  public boolean setDueDate(String dueDate){
+    this.dueDate =  dueDate;
+    return true;
+  }
+  public void checkout(String patron, String due){
+
+  }
+  public void returned(){
+
+  }
+  public String circulationStatus(){
+    return "";
+  }
+}
