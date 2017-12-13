4f439937a330ad7ffdcc03c4011a153c2e91421c
DenChen11214 <dchen22@stuy.edu>
2017-12-13 14:47:23 -0500

    created the temperaturewindow constructor setting buttons and textboxes, added them to the actionlistener and pane

---
+++ 09GUI/TemperatureWindow.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class TemperatureWindow extends JFrame implements ActionListener{
+    private JButton fcButton;
+    private JButton cfButton;
+    private JTextField fcText;
+    private JTextField cfText;
+    private Container pane;
+    public TemperatureWindow(){
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
+	this.setTitle("TemperatureConverter");
+	this.setSize(600,400);
+	this.setLocation(0,0);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	fcButton = new JButton("FahrenheitToCelcius");
+	cfButton = new JButton("CelciustoFahrenheit");
+	fcText = new JTextField(15);
+	cfText = new JTextField(15);
+	fcButton.addActionListener(this);
+	cfButton.addActionListener(this);
+	pane.add(fcButton);
+	pane.add(fcText);
+	pane.add(cfButton);
+	pane.add(cfText);
+    }
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
