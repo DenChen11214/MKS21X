81978383508b45d8cf80135bb96ff7db54ffc283
Dennis Chen <dchen22@stuy.edu>
2017-12-13 18:18:27 -0500

    made the text nicer, added the main, fixed the settext in actionsperformed

--- 09GUI/TemperatureWindow.java
+++ 09GUI/TemperatureWindow.java
@@ -11,11 +11,11 @@ public class TemperatureWindow extends JFrame implements ActionListener{
     pane = this.getContentPane();
     pane.setLayout(new FlowLayout());
     this.setTitle("TemperatureConverter");
-    this.setSize(600,400);
+    this.setSize(500,400);
     this.setLocation(0,0);
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    fcButton = new JButton("FahrenheitToCelcius");
-    cfButton = new JButton("CelciustoFahrenheit");
+    fcButton = new JButton("Fahrenheit To Celcius");
+    cfButton = new JButton("Celcius To Fahrenheit");
     fcText = new JTextField(15);
     cfText = new JTextField(15);
     fcButton.addActionListener(this);
@@ -26,12 +26,22 @@ public class TemperatureWindow extends JFrame implements ActionListener{
     pane.add(cfText);
   }
   public void actionPerformed(ActionEvent e){
-
+    String s = e.getActionCommand();
+    if(s.equals("Fahrenheit To Celcius")){
+      fcText.setText(Double.toString(FtoC(Double.parseDouble(fcText.getText()))));
+    }
+    if(s.equals("Celcius To Fahrenheit")){
+      cfText.setText(Double.toString(CtoF(Double.parseDouble(cfText.getText()))));
+    }
   }
   public double CtoF(double cel){
     return cel * 1.8 + 32;
   }
   public double FtoC(double fah){
     return (fah - 32) / 1.8;
+  }
+  public static void main(String[] args){
+    TemperatureWindow t = new TemperatureWindow();
+    t.setVisible(true);
   }
 }
