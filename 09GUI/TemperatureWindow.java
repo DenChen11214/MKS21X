94bc68483106ec93f429e9053c2c2cd2150260d0
DenChen11214 <dchen22@stuy.edu>
2017-12-14 14:24:42 -0500

    added try and catch, deleted a text box

--- 09GUI/TemperatureWindow.java
+++ 09GUI/TemperatureWindow.java
@@ -11,27 +11,30 @@ public class TemperatureWindow extends JFrame implements ActionListener{
     pane = this.getContentPane();
     pane.setLayout(new FlowLayout());
     this.setTitle("TemperatureConverter");
-    this.setSize(500,400);
+    this.setSize(600,400);
     this.setLocation(0,0);
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     fcButton = new JButton("Fahrenheit To Celcius");
     cfButton = new JButton("Celcius To Fahrenheit");
     fcText = new JTextField(15);
-    cfText = new JTextField(15);
     fcButton.addActionListener(this);
     cfButton.addActionListener(this);
     pane.add(fcButton);
     pane.add(fcText);
     pane.add(cfButton);
-    pane.add(cfText);
   }
   public void actionPerformed(ActionEvent e){
     String s = e.getActionCommand();
-    if(s.equals("Fahrenheit To Celcius")){
-      fcText.setText(Double.toString(FtoC(Double.parseDouble(fcText.getText()))));
+    try{
+	if(s.equals("Fahrenheit To Celcius")){
+	    fcText.setText(Double.toString(FtoC(Double.parseDouble(fcText.getText()))));
+	}
+	if(s.equals("Celcius To Fahrenheit")){
+	    fcText.setText(Double.toString(CtoF(Double.parseDouble(cfText.getText()))));
+	}
     }
-    if(s.equals("Celcius To Fahrenheit")){
-      cfText.setText(Double.toString(CtoF(Double.parseDouble(cfText.getText()))));
+    catch(NumberFormatException error){
+	fcText.setText("Double type Only");
     }
   }
   public double CtoF(double cel){
